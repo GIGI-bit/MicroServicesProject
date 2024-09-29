76550bf5289e797cc8fa566e2bb18f5fae54ed3f
elesg_za05 <elesg_za05@STHQ0123-15>
2024-09-29 12:01:01 +0400

    Add project files.

---
+++ Contact.API.Services/ContactService.cs
@@ -0,0 +1,45 @@
+﻿using Contact.API.Infrastructure;
+using Contact.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact.API.Services
+{
+    public class ContactService:IContactService
+    {
+        private List<ContactModel> AllContacts { get; set; }
+        public ContactService()
+        {
+            
+        }
+
+        public void Add(API.Models.ContactModel model)
+        {
+            model.Id = new Random().Next(1, 10000);
+            AllContacts.Add(model);
+        }
+
+        public void Delete(int id)
+        {
+            //var item = AllContacts.Remove();
+        }
+
+        public List<API.Models.ContactModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public API.Models.ContactModel GetContactById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, API.Models.ContactModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
